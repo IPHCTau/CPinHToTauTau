40f2245247c6919e9f44001bf5c8790642da58c5
gourab saha <gourab.saha@cern.ch>
2025-05-29 22:13:06 +0200

    modified a bit because a mismatch in shape observed between arg0 and arg1 for a branch119 in mu1_C 2023 dataset

--- httcp/calibration/electron.py
+++ httcp/calibration/electron.py
@@ -24,27 +24,36 @@ set_ak_column_f32 = functools.partial(set_ak_column, value_type=np.float32)
 
 @calibrator(
     uses={"run",
-          "Electron.pt","Electron.r9","Electron.eta", "Electron.seedGain", "Electron.scEtOverPt",
+          "Electron.pt","Electron.r9", "Electron.eta", "Electron.deltaEtaSC",
+          "Electron.seedGain", "Electron.scEtOverPt", "Electron.ecalEnergy",
     },
     produces={
+        #"Electron.rawPt",
         "Electron.pt",
     },
     mc_only=False,
 )
 def electron_smearing_scaling(self: Calibrator, events: ak.Array, **kwargs) -> ak.Array:
     # fail when running on data
+    gain = flat_np_view(events.Electron.seedGain, axis=1)
+    _, run_brdcst = ak.broadcast_arrays(
+        events.Electron.pt, events.run,
+    )
+    run = flat_np_view(run_brdcst, axis=1)
+    eta = flat_np_view((events.Electron.eta + events.Electron.deltaEtaSC), axis=1)
+    r9  = flat_np_view(events.Electron.r9, axis=1)
+    #et = events.Electron.scEtOverPt #real
+    et  = flat_np_view(events.Electron.pt, axis=1)
+    ecalEn = flat_np_view(events.Electron.ecalEnergy, axis=1)
+    
+    electron_pt = events.Electron.pt
+    electron_pt_shape = ak.num(electron_pt, axis=1)
+    
     if self.dataset_inst.is_data:    
         syst = "total_correction" 
-        gain = events.Electron.seedGain #int
-        run = events.run #real
-        eta = events.Electron.eta #real
-        r9 = events.Electron.r9 #real
-        et = events.Electron.scEtOverPt #real
 
-        electron_pt = events.Electron.pt
-        
         #Create get energy scale correction for each tau
-        electron_scaling_nom = np.ones_like(electron_pt, dtype=np.float32)
+        #electron_scaling_nom = np.ones_like(et, dtype=np.float32)
 
         if self.config_inst.campaign.x.run == 3:
             electron_scaling_args = lambda events, syst: (syst,
@@ -55,17 +64,18 @@ def electron_smearing_scaling(self: Calibrator, events: ak.Array, **kwargs) -> a
                                                           et)
 
         electron_scaling_nom = self.electron_scaling_corrector.evaluate(*electron_scaling_args(events, syst))
-
-        events = set_ak_column_f32(events, "Electron.pt", electron_pt * electron_scaling_nom)
+        electron_pt_corr_flat = electron_scaling_nom * et
+        electron_ecal_energy_corr_flat = electron_scaling_nom * ecalEn
+        events = set_ak_column_f32(events,
+                                   "Electron.pt",
+                                   ak.unflatten(electron_pt_corr_flat, electron_pt_shape))
     
     elif self.dataset_inst.is_mc:
         
         syst = "rho"  
-        eta =events.Electron.eta 
-        r9 = events.Electron.r9 
-        electron_pt = events.Electron.pt
+
         #Create get energy scale correction for each electron
-        electron_smearing_nom = np.ones_like(electron_pt, dtype=np.float32)
+        #electron_smearing_nom = np.ones_like(electron_pt, dtype=np.float32)
 
         if self.config_inst.campaign.x.run == 3:
             electron_smearing_args = lambda events, syst: (syst,
@@ -75,15 +85,17 @@ def electron_smearing_scaling(self: Calibrator, events: ak.Array, **kwargs) -> a
         electron_smearing_nom = self.electron_smearing_corrector.evaluate(*electron_smearing_args(events, syst))
         rng = np.random.default_rng(seed=125)  
         
-        electron_smearing_nom_flat      = np.asarray(ak.flatten(electron_smearing_nom))
-        electron_pt      = np.asarray(ak.flatten(events.Electron.pt))
-        arr_shape = ak.num(events.Electron.pt, axis=1)
+        #electron_smearing_nom_flat      = np.asarray(ak.flatten(electron_smearing_nom))
+        #electron_pt      = np.asarray(ak.flatten(events.Electron.pt))
+        #arr_shape = ak.num(events.Electron.pt, axis=1)
 
         # Apply random smearing
-        smearing = rng.normal(loc=1., scale=electron_smearing_nom_flat)
-        
+        smearing = rng.normal(loc=1., scale=electron_smearing_nom)
 
-        events = set_ak_column_f32(events, "Electron.pt", ak.unflatten(electron_pt * smearing, arr_shape))
+        electron_pt_corr_flat = smearing * et
+        electron_ecal_energy_corr_flat = smearing * ecalEn
+        
+        events = set_ak_column_f32(events, "Electron.pt", ak.unflatten(electron_pt_corr_flat, electron_pt_shape))
 
     return events
 
